56d5b8cfc07ff76e4b4ddaeed8344fae49902629
ragel-ter <rachel@boltonbuilt.co.za>
2023-09-30 14:00:10 +0200

    Added directoryController.js module and dependecies. Dependencies still need fleshing out and currently contain placeholder functions.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const restify = require('restify');
-
 const server = restify.createServer();
+const DocumentController = require('./controllers/directoryController')
 
 server.get('/ping', function(req, res, next) {
     res.send('pong');
     return next();
 })
+
+server.get('/:path/ls', DocumentController.getListing);
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
